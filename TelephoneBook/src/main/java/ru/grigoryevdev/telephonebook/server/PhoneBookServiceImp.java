ed190ecf83ee4f9f35eede4546944e76d7bce13c
dmitriygg <dmitriygg@gmail.com>
2014-07-01 12:51:16 +0400

    3.1

--- TelephoneBook/src/main/java/ru/grigoryevdev/telephonebook/server/PhoneBookServiceImp.java
+++ TelephoneBook/src/main/java/ru/grigoryevdev/telephonebook/server/PhoneBookServiceImp.java
@@ -7,7 +7,9 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import ru.grigoryevdev.telephonebook.server.dao.PhoneBookDAO;
+import ru.grigoryevdev.telephonebook.server.dao.StreetDAO;
 import ru.grigoryevdev.telephonebook.shared.domain.Phone;
+import ru.grigoryevdev.telephonebook.shared.domain.Street;
 import ru.grigoryevdev.telephonebook.shared.service.PhoneBookService;
 
 @Service("phoneBookService")
@@ -15,6 +17,9 @@ public class PhoneBookServiceImp implements PhoneBookService {
 
 	@Autowired
 	private PhoneBookDAO phoneBookDAO;
+	
+	@Autowired
+	private StreetDAO streetDAO;	
 
 	@Transactional
 	public void addPhone(Phone phone) {
@@ -29,6 +34,19 @@ public class PhoneBookServiceImp implements PhoneBookService {
 	@Transactional
 	public void removePhone(Integer id) {
 		phoneBookDAO.removePhone(id);
+	}
+    
+	@Transactional
+	public void addStreet(Street street) {
+		streetDAO.addStreet(street);		
+	}
+	@Transactional
+	public List<Street> listStreet() {
+		return streetDAO.listStreet();
+	}
+	@Transactional
+	public void removeStreet(Integer id) {
+		streetDAO.removeStreet(id);		
 	}
 
 }
